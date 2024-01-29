30fb3d092921831d0700715315c371347828af4d
Victory Ifebhor <victory.ifebhor@gmail.com>
2024-01-29 15:06:00 +0100

    The about component has now been rendered in the main page

--- src/components/content.jsx
+++ src/components/content.jsx
@@ -1,9 +1,11 @@
+import { About } from "./about";
 import { ContentHeader } from "./contentHeader";
 import { Courses } from "./courses";
 
 export const Content = () => {
   return (
     <main>
+      <About />
       <ContentHeader />
       <Courses />
     </main>
