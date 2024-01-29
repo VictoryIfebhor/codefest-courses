759cc8d34d549c706da446fc5b0d9c3d9029e44e
Victory Ifebhor <victory.ifebhor@gmail.com>
2024-01-29 15:01:01 +0100

    Added about us section to the page

---
+++ src/components/about.jsx
@@ -0,0 +1,23 @@
+import { ContentSection } from "./contentSection";
+
+export const About = () => {
+  return (
+    <ContentSection>
+      <h2>About us</h2>
+      <p>
+        <span>
+          An organization birthed to transform Africa economy and positioned in
+          Africa to bring about collaboration and accessibility; innovation and
+          speed; change and management. This drive started since 2010, we are
+          striving to grow the largest knowledge force, made up of an
+          institution of passionate people with a common goal to make a
+          significant contribution in the technology and business landscape of
+          Africa.
+        </span>
+        <a href="https://codefestuniversity.online" className="codefest-link">
+          Read more
+        </a>
+      </p>
+    </ContentSection>
+  );
+};
