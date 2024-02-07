df6f79095fe87f9179db1a77876bb25b796e40ad
Victory Ifebhor <victory.ifebhor@gmail.com>
2024-02-07 16:41:14 +0100

    Social media accounts have been added to footer

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -4,12 +4,18 @@ export const Footer = () => {
       <div className="left-footer">
         <img
           src="https://codefestuniversity.online/assets/logo-fa6ae0c9.png"
-          alt=""
+          alt="Company Logo"
         />
       </div>
       <div className="right-footer">
-        <h6>Contact us:</h6>
-        <p>08012345678</p>
+        <div className="contact-info">
+          <p>Phone: 08012345678</p>
+          <div className="social-media">
+            <a href="https://www.instagram.com">Instagram</a>
+            <a href="https://www.facebook.com">Facebook</a>
+            <a href="https://www.twitter.com">Twitter</a>
+          </div>
+        </div>
       </div>
     </footer>
   );
