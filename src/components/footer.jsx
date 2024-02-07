ba732f44b3325008cf39a7c4b5b83b3216e53957
Victory Ifebhor <victory.ifebhor@gmail.com>
2024-02-07 17:15:09 +0100

    Set link for social media to go to correct page

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -11,9 +11,24 @@ export const Footer = () => {
         <div className="contact-info">
           <p>Phone: 08012345678</p>
           <div className="social-media">
-            <a href="https://www.instagram.com">Instagram</a>
-            <a href="https://www.facebook.com">Facebook</a>
-            <a href="https://www.twitter.com">Twitter</a>
+            <a
+              href="https://www.instagram.com/codefestinstitute_technology/"
+              target="_blank"
+            >
+              Instagram
+            </a>
+            <a
+              href="https://web.facebook.com/profile.php?id=61555518131948"
+              target="_blank"
+            >
+              Facebook
+            </a>
+            <a
+              href="https://www.linkedin.com/company/codefestinstitute/"
+              target="_blank"
+            >
+              LinkedIn
+            </a>
           </div>
         </div>
       </div>
