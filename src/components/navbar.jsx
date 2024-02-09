a974218185615bf213fbcfbbfc74bfcabe08cb89
Victory Ifebhor <victory.ifebhor@gmail.com>
2024-02-09 08:20:39 +0100

    Changed nav bar logo

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,14 +1,16 @@
 export const Navbar = () => {
   return (
     <nav>
-      <div className="left-nav nav-item">
-        <img
-          src="https://codefestuniversity.online/assets/logo-fa6ae0c9.png"
-          alt=""
-        />
+      <div className="left-nav">
+        <a href="">
+          <img
+            src="https://media.licdn.com/dms/image/D4D0BAQHkkC2HBf3xkw/company-logo_200_200/0/1705705169888/e_woke_logo?e=1715817600&v=beta&t=EMNLhMFH0vCe8r8f3dTynxF4Tayh7lv87J7cPOO1fX8"
+            alt=""
+          />
+        </a>
       </div>
       <div className="right-nav">
-        <div className="menus nav-item">
+        <div className="menus">
           <span>
             <a href="">home</a>
           </span>
